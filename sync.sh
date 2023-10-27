f38cb4a458b301eb2427424aca30075f981820b5
antsmallant <412288482@qq.com>
2023-10-27 16:58:11 +0800

    sync

--- sync.sh
+++ sync.sh
@@ -6,3 +6,4 @@ git add ./
 git commit -m "sync"
 git push github
 git push gitee master
+git push local-gitlab master
--- win_sync.bat
+++ win_sync.bat
@@ -3,5 +3,6 @@ git pull github master
 git add ./
 git commit -m "sync"
 git push github
-
+git push gitee master
+git push local-gitlab master
 @pause
